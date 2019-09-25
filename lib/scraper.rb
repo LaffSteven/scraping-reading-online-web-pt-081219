c30537c472cf3603e0b1237b904b457b55f90116
Steven Laff <steve.laff@gmail.com>
2019-09-25 22:19:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,10 +4,10 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
-#doc.css(".headline-26OIBN").text
+#doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G").text
 
 
-# courses = doc.css(".title-oE5vT4")
+# courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
  courses.each do |course|
      puts course.text.strip
